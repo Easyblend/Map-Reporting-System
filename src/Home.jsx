09d2a9a3f417872a66a8c0a29fd4bccb2d4949f9
easyblend <kenzieemma072@gmail.com>
2022-11-26 22:42:24 +0000

    Log feature enabled on the homePage component

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,18 +2,31 @@ import React, { useState } from "react";
 // import { useContext } from "react";  ****This Context wasnt Needed onauthstateChange checks and updates the user logged in
 // import { userState } from "./UserStateContext";
 import { authentication } from "./FirebaseConfig";
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+import { Button } from "react-bootstrap";
 
 const Home = () => {
   //Checking the current user logged in my databse
-
+  const navigate = useNavigate();
   onAuthStateChanged(authentication, (currentUser) => {
-    setUserEmail(currentUser.email);
+    setUserEmail(currentUser?.email);
   });
+
+  const logOut = async () => {
+    await signOut(authentication);
+    navigate("/");
+  };
 
   //   const { userEmail, setUserEmail } = useContext(userState);
   const [userEmail, setUserEmail] = useState("");
-  return <h2>Current user Logged in: {userEmail} </h2>;
+  return (
+    <div className="text-center">
+      <h2>Current user Logged in: {userEmail ? userEmail : "guest"}</h2>
+      <Button onClick={logOut}>Log me Out</Button>
+    </div>
+  );
 };
 
 export default Home;
