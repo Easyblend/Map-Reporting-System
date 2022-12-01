cdab35a6daf744335dc32dea720214e910459c52
easyblend <kenzieemma072@gmail.com>
2022-12-01 15:56:00 +0000

    Separated the navbar into component and added profile navlinks

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,67 +1,10 @@
-import React, { useEffect, useState } from "react";
-
-import { authentication } from "./FirebaseConfig";
-import { onAuthStateChanged, signOut } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
-import Nav from "react-bootstrap/Nav";
-import { Button } from "react-bootstrap";
-import Container from "react-bootstrap/Container";
-
+import React from "react";
+import NavbarComponent from "./NavbarComponent";
 const Home = () => {
-  //Checking the current user logged in my databse
-  const func = async () => {
-    await onAuthStateChanged(authentication, (currentUser) => {
-      setUserEmail(currentUser?.email);
-      setUserName(currentUser?.displayName);
-      console.log(authentication.currentUser);
-    });
-  };
-
-  useEffect(() => {
-    func();
-  }, []);
-
-  const navigate = useNavigate();
-
-  const logOut = async () => {
-    try {
-      await signOut(authentication);
-
-      navigate("/");
-    } catch (error) {
-      alert(error);
-    }
-  };
-
-  //   const { userEmail, setUserEmail } = useContext(userState);
-  const [userEmail, setUserEmail] = useState("");
-  const [userName, setUserName] = useState("");
   return (
-    <>
-      <Navbar
-        collapseOnSelect
-        expand="md"
-        bg="light"
-        className="mx-auto w-100 text-center"
-      >
-        <Container className="container-fluid">
-          <Navbar.Brand href="">{userName ? userName : "GUEST"}</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            {/* <Nav className="ms-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav> */}
-            <Nav className="ms-auto">
-              <Nav.Link>
-                <Button onClick={logOut}>Log me Out</Button>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+    <div>
+      <NavbarComponent />
+    </div>
   );
 };
 
---
+++ src/Login.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+import { useRef } from "react";
+import { Button } from "react-bootstrap";
+import {
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { authentication } from "./FirebaseConfig";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+
+const Login = () => {
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const navigate = useNavigate();
+  const SubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(
+        authentication,
+        email.current.value,
+        password.current.value
+      );
+      navigate("/home");
+      toast.success("Login Successful");
+    } catch (error) {
+      const errorCode = error.code;
+      toast.error(
+        errorCode === "auth/invalid-email"
+          ? "Invalid Email"
+          : errorCode === "auth/user-not-found"
+          ? "No Account with this Email"
+          : errorCode === "auth/wrong-password"
+          ? "Wrong Password"
+          : "Somethings's not right"
+      );
+    }
+  };
+
+  const passwordReset = async () => {
+    try {
+      await sendPasswordResetEmail(authentication, email.current.value);
+      toast.success("Password Reset Successfull. Please check your email");
+    } catch (error) {
+      console.log(error.code);
+      toast.error(
+        error.code === "auth/missing-email"
+          ? "Please Enter your Email to reset your password"
+          : error.code === "auth/user-not-found"
+          ? "There is no account with this email"
+          : "Something went wrong"
+      );
+    }
+  };
+
+  return (
+    <div className="row mx-auto bg-light vh-100 text-dark">
+      <div className="col-sm-6 my-auto col-12 mx-auto">
+        <h2 className="text-center">Log In</h2>
+        <Form
+          className="w-75 mx-auto mt-5 d-flex flex-column gap-5"
+          onSubmit={SubmitForm}
+        >
+          <Form.Group>
+            <Form.Control
+              type="email"
+              placeholder="Email.."
+              ref={email}
+              required
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Control
+              type="password"
+              placeholder="Password.."
+              ref={password}
+              required
+            />
+          </Form.Group>
+          <Button type="submit">Log In</Button>
+        </Form>
+        <div className="text-center mt-3">
+          New Here?
+          <Link
+            to="/"
+            className="text-decoration-none fw-bold mx-1 text-danger ms-2"
+          >
+            Create Account
+          </Link>
+        </div>
+        <div className="text-center mt-4">
+          Forgot Password?
+          <span
+            className="fw-bolder ms-2"
+            role="button"
+            onClick={passwordReset}
+          >
+            Click Here
+          </span>
+        </div>
+      </div>
+
+      <img
+        src="https://images.unsplash.com/photo-1604077198996-4eb67c32f6a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        alt="Banner Image"
+        className="col-6 banner d-none d-sm-block px-0"
+      />
+    </div>
+  );
+};
+
+export default Login;
