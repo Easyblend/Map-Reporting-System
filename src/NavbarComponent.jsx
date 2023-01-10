d78116d69d5276fd454bfe078935bf0f3015ff37
easyblend <kenzieemma072@gmail.com>
2023-01-10 04:20:37 +0000

    added data to the sidebar

--- src/NavbarComponent.jsx
+++ src/NavbarComponent.jsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from "react";
 
 import { authentication } from "./FirebaseConfig";
 import { onAuthStateChanged, signOut } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useMapEvent } from "react-leaflet";
 import { useContext } from "react";
 import { userState } from "./UserStateContext";
 import { toast } from "react-toastify";
+import { Button } from "react-bootstrap";
 
 const NavbarComponent = ({ getUser }) => {
   //Checking the current user logged in my databse
@@ -54,9 +55,9 @@ const NavbarComponent = ({ getUser }) => {
         aria-labelledby="offcanvasRightLabel"
       >
         <div className="offcanvas-header">
-          <h5 className="offcanvas-title" id="offcanvasRightLabel">
+          <h6 className="offcanvas-title" id="offcanvasRightLabel">
             {userName}
-          </h5>
+          </h6>
           <button
             type="button"
             className="btn-close ms-5"
@@ -64,7 +65,54 @@ const NavbarComponent = ({ getUser }) => {
             aria-label="Close"
           ></button>
         </div>
-        <div className="offcanvas-body"></div>
+        <div className="text-center">
+          <div className="my-3 py-2">
+            <i class="fa-solid fa-house"></i>{" "}
+            <Link
+              to="/home"
+              className="text-center text-decoration-none text-secondary mx-4"
+            >
+              Home
+            </Link>
+          </div>
+          <div className="my-3 py-2">
+            <i class="fa-solid fa-circle-info"></i>
+            <Link
+              to="/home"
+              className="text-center text-decoration-none text-secondary mx-4"
+            >
+              About
+            </Link>
+          </div>
+          <div className="my-3 py-2">
+            <i class="fa-solid fa-user"></i>
+            <Link
+              to="/home"
+              className="text-center text-decoration-none text-secondary mx-4"
+            >
+              Profile
+            </Link>
+          </div>
+
+          <div className="my-3 py-2">
+            <Button className="text-center bg-danger border border-0 shadow-sm">
+              Log Out <i class="fa-solid fa-right-from-bracket"></i>
+            </Button>
+          </div>
+        </div>
+        <div className="text-center mt-4">
+          {" "}
+          <p className="fs-5">Actions</p>
+          <div className="my-1 ">
+            <i class="fa-solid fa-person-circle-exclamation"></i>
+            <Link
+              to="/home"
+              className="text-center text-decoration-none text-secondary mx-4"
+            >
+              Reports
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
